2843326ad0a29353c28dbd0e1971b82b0ffe4056
Yash Kumar <hypnoastic@gmail.com>
2025-05-01 18:14:23 +0530

    modified

--- main.py
+++ main.py
@@ -1,31 +1,24 @@
 # Imports
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-
 data = pd.read_csv("titanic.csv")
 data.info()
 print(data.isnull().sum())
 
-
 # Data Cleaning
 def preprocess_data(df):
     df.drop(columns=["PassengerId", "Name", "Ticket", "Cabin"], inplace=True)
-
     df["Embarked"].fillna("S")
     df.drop(columns=["Embarked"], inplace=True)
-
     fill_missing_ages(df)
-
     df["Sex"] = df["Sex"].map({'male': 1, "female": 0})
-
     df["FamilySize"] = df["SibSp"] + df["Parch"]
     df["IsAlone"] = np.where(df["FamilySize"] == 0, 1, 0)
     df["FareBin"] = pd.qcut(df["Fare"], 4, labels=False)
@@ -40,9 +33,7 @@ def fill_missing_ages(df):
     for pclass in df["Pclass"].unique():
         if pclass not in age_fill_map:
             age_fill_map[pclass] = df[df["Pclass"] == pclass]["Age"].median()
-
     df["Age"] = df.apply(lambda row: age_fill_map[row["Pclass"]] if pd.isnull(row["Age"]) else row["Age"], axis=1)
-
 
 data = preprocess_data(data)
 X = data.drop(columns=["Survived"])
@@ -55,7 +46,6 @@ scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-
 # Hyperparemeter Tuning - KNN
 def tune_model(X_train, y_train):
     param_grid = {
@@ -63,15 +53,12 @@ def tune_model(X_train, y_train):
         "metric": ["euclidean", "manhattan", "minkowski"],
         "weights": ["uniform", "distance"]
     }
-
     model = KNeighborsClassifier()
     grid_search = GridSearchCV(model, param_grid, cv=5, n_jobs=-1)
     grid_search.fit(X_train, y_train)
     return grid_search.best_estimator_
 
-
 best_model = tune_model(X_train, y_train)
-
 
 # Predictions and evaluate
 def evaluate_model(model, X_test, y_test):
@@ -80,13 +67,10 @@ def evaluate_model(model, X_test, y_test):
     matrix = confusion_matrix(y_test, prediction)
     return accuracy, matrix
 
-
 accuracy, matrix = evaluate_model(best_model, X_test, y_test)
-
 print(f'Accuracy: {accuracy * 100:.2f}%')
 print(f'Confusion Matrix:')
 print(matrix)
-
 
 # Plot
 def plot_model(matrix):
